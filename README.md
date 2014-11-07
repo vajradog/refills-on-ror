084dbecbf52f9474abdf01181b56f7d52ce65308
vajradog <vajradog@gmail.com>
2014-11-07 10:16:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+== README
+
+ruby '2.1.0'
+gem 'rails', '4.1.4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_refills_on_ror_session'
